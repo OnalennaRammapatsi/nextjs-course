ba20d26528eb3ba9b127ce059072a242db05daaf
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-09-18 09:59:28 +0200

    updated comment-list

--- nextjs-tutorial/components/helpers/api-util.js
+++ nextjs-tutorial/components/helpers/api-util.js
@@ -1,5 +1,5 @@
 
-
+//this funtion fetches a list of events 
 export async function getAllEvents(){
 const response = await fetch('https://nextjs-course-9ec81-default-rtdb.firebaseio.com/events.json');
 const data = await response.json();
@@ -16,23 +16,23 @@ for  (const key in data) {
 return events;
 } 
 
-
+//filters them to return only events marked featured.
 export async function getFeaturedEvents() {
     const allEvents = await getAllEvents();
     return allEvents.filter((event) => event.isFeatured);
   }
-
+//search for specific event  by ID
   export async function getEventById(id) {
     const allEvents = await getAllEvents();
     return allEvents.find((event) => event.id === id);
   }
-
+//
   export async function getFilteredEvents(dateFilter) {
     const { year, month } = dateFilter;
   
 
     const allEvents = await getAllEvents();
-
+//converts the events date and compares the year and month
     let filteredEvents = allEvents.filter((event) => {
       const eventDate = new Date(event.date);
       return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
